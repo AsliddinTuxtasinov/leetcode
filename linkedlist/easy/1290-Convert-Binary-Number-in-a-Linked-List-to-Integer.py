dac4226e4f2cbb35e9a829f68b2bf387ccc92a85
AsliddinTuxtasinov <asliddintukhtasinov5@gmail.com>
2023-11-28 18:12:30 +0500

    18-solved(1290-Convert-Binary-Number-in-a-Linked-List-to-Integer) easy!

--- linkedlist/easy/1290-Convert-Binary-Number-in-a-Linked-List-to-Integer.py
+++ linkedlist/easy/1290-Convert-Binary-Number-in-a-Linked-List-to-Integer.py
@@ -22,9 +22,9 @@ class ListNode:
 def create_linked_list(arr: list) -> ListNode:
     if arr is None:
         return None
-    
+
     head = ListNode(val=arr[0])
-    
+
     current_node = head
     for val in arr[1:]:
         current_node.next = ListNode(val=val)
@@ -32,18 +32,19 @@ def create_linked_list(arr: list) -> ListNode:
 
     return head
 
+
 # Function to get all values from the linked list
 def get_all_nodes_values(linked_list: ListNode) -> Optional[list]:
     res = []
     if linked_list is None:
         return res
-    
+
     current_node = linked_list
     # Traverse the linked list and append values to the result list
     while current_node.next:
         res.append(current_node.val)
         current_node = current_node.next
-    
+
     # Append the value of the last node
     res.append(current_node.val)
     return res
@@ -55,15 +56,15 @@ class Solution:
         arr = get_all_nodes_values(linked_list=head)
         # Reverse the list to process it from LSB to MSB
         arr.reverse()
-        
+
         res = 0
         # Calculate the decimal value using the reversed list
         for index in range(len(arr)):
-            res = res + arr[index] * (2**index) 
+            res = res + arr[index] * (2**index)
         return res
 
 
-head = create_linked_list(arr=[1,0,1])
+head = create_linked_list(arr=[1, 0, 1])
 # head = create_linked_list(arr=[0])
 
 print(Solution().getDecimalValue(head=head))
